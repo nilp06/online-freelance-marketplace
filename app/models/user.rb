a9bde2bd86f20e3252789d90f388b734d20af8d2
Nil Patel <125335891+nilp06@users.noreply.github.com>
2023-07-06 14:05:56 +0530

    refactor : update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum :role, %i[freelancer client]
-  validates :role, presence: true
-  validates :user_name, presence: true
-  validates :mobile_number, format: { with: /\A[5-9][0-9]{9}\Z/, message: 'is invalid.' }
+  validates_presence_of %i[role user_name]
+  validates :mobile_number, presence: true, uniqueness: true,
+                            format: { with: /\A[5-9][0-9]{9}\Z/, message: 'is invalid.' }
 end
